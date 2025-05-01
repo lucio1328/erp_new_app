3d840014fd6bd06acbb87a83c0c78fd5d8a63132
Lucio Rakotoniaina <153309163+lucio1328@users.noreply.github.com>
2025-05-01 10:58:47 +0200

    structure du projet

---
+++ src/main/java/com/lucio/erp_new_app/ErpNewAppApplication.java
@@ -0,0 +1,13 @@
+package com.lucio.erp_new_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpNewAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpNewAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucio/erp_new_app/ErpNewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucio.erp_new_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpNewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
